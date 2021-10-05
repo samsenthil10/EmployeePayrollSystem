70ea2507eaa656eae452b5cd55e411b17b1d6bf5
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-06 04:40:54 +0530

    Refactored: To get working hours by method

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,34 +1,29 @@
-const IS_ABSENT=0
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+const NO_OF_WRK_DAYS = 20;
+const MAX_WRK_HRS = 160;
 
-let employeeCheck=Math.floor(Math.random()*10)%2;
 
-if(employeeCheck==IS_ABSENT) {
-    console.log("Employee is Absent");
-    return
+function getWorkHRs(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
+
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
+
+        default:
+            return 0;
+    }
 }
-
-else {
-    console.log("Employee is Present");
+let total_work_hours = 0, total_wage = 0, empCheck = 0, days = 0;
+while (days <= NO_OF_WRK_DAYS && total_work_hours <= MAX_WRK_HRS) {
+    days++;
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    total_work_hours += getWorkHRs(empCheck);
 }
-
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
-
-let employeeHours=0;
-let employeeCheck=Math.floor(Math.random()*10)%3;
-switch(employeeCheck) {
-    case IS_PART_TIME:
-        employeeHours=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        employeeHours=FULL_TIME_HOURS;
-        break;
-    default:
-        employeeHours=0;
-}
-
-let employeeWage=employeeHours*WAGE_PER_HOUR;
-console.log("Employee wage: "+employeeWage);
+total_wage = WAGE_PER_HR * total_work_hours;
+console.log("Wage = " + total_wage + " WorkHrs = " + total_work_hours);
