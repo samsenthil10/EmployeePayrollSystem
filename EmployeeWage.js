95ed5cd15ec8ba32d53b47e2e7626fed51ecd33e
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-06 04:49:48 +0530

    Modified: To terminate loop in case of total work hours or max days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,8 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
-const NO_OF_WRK_DAYS = 20;
-const MAX_WRK_HRS = 160;
+const NO_OF_WORK_DAYS = 20;
+const MAX_WORK_HRS = 160;
 
 
 function getWorkHRs(employeeCheck) {
@@ -19,11 +19,14 @@ function getWorkHRs(employeeCheck) {
             return 0;
     }
 }
-let total_work_hours = 0, total_wage = 0, employeeCheck = 0, days = 0;
-while (days <= NO_OF_WRK_DAYS && total_work_hours <= MAX_WRK_HRS) {
+
+let totalWorkHours = 0, totalWage = 0, employeeCheck = 0, days = 0;
+
+while (days <= NO_OF_WORK_DAYS && totalWorkHours <= MAX_WORK_HRS) {
     days++;
-    employeeCheck = Math.floor(Math.random() * 10) % 3;
-    total_work_hours += getWorkHRs(employeeCheck);
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    totalWorkHours += getWorkHRs(empCheck);
 }
-total_wage = WAGE_PER_HR * total_work_hours;
-console.log("Wage = " + total_wage + " Work Hours = " + total_work_hours);
+
+totalWage = WAGE_PER_HR * totalWorkHours;
+console.log("Wage = " + totalWage + " Work Hours = " + totalWorkHours);
