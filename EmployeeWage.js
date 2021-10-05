ffb03f32f6cc1ec4e06f2abb6c19d142b0f4308f
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-06 04:37:10 +0530

    Added: To find wage based on part time or full time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -10,3 +10,25 @@ if(employeeCheck==IS_ABSENT) {
 else {
     console.log("Employee is Present");
 }
+
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+
+let employeeHours=0;
+let employeeCheck=Math.floor(Math.random()*10)%3;
+switch(employeeCheck) {
+    case IS_PART_TIME:
+        employeeHours=PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        employeeHours=FULL_TIME_HOURS;
+        break;
+    default:
+        employeeHours=0;
+}
+
+let employeeWage=employeeHours*WAGE_PER_HOUR;
+console.log("Employee wage: "+employeeWage);
